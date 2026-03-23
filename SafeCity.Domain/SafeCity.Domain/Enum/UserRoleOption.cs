7eac65b48f6f99dd5e29293f566a6f4ee90c8531
Himanshu <HimanshuJaswant.Sadhwani@cognizant.com>
2026-03-23 15:18:26 +0530

    Solved connection string issue

--- SafeCity.Domain/SafeCity.Domain/Enum/UserRoleOption.cs
+++ SafeCity.Domain/SafeCity.Domain/Enum/UserRoleOption.cs
@@ -1,5 +1,22 @@
-﻿namespace SafeCity.Domain.Enum
-{
-    public enum UserRoleOption { Admin = 1, Officer = 2, Dispatcher = 3, Citizen = 4 }
-}
+﻿using System.ComponentModel.DataAnnotations;
 
+public enum UserRoleOption
+{
+    [Display(Name="Citizen")]
+    Citizen = 1,
+
+    [Display(Name="Police Officer")]
+    Police = 2,
+
+    [Display(Name="Fire Fighter")]
+    Fire_Fighter = 3,
+
+    [Display(Name="Emergency Dispatcher")]
+    Emergency_Dispatcher = 4,
+
+    [Display(Name="Compliance Officer")]
+    Compliance_Officer = 5,
+
+    [Display(Name="City Administrator")]
+    City_Administrator = 6
+}
