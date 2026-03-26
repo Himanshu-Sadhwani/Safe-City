3ae469794638b75ba248fe622d1f85e9b910f792
Charishma <Charishma.Thota@cognizant.com>
2026-03-26 23:00:26 +0530

    Added Controller for User Update

--- SafeCity/SafeCity/Controllers/UserController.cs
+++ SafeCity/SafeCity/Controllers/UserController.cs
@@ -2,11 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 using SafeCity.DTOs;
 using SafeCity.Services;
 using SafeCity.Utility;
-
 namespace SafeCity.Controllers
 {
     [Route("api/v1/[controller]")]
     [ApiController]
+
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
@@ -14,7 +14,6 @@ namespace SafeCity.Controllers
         {
             _userService = userService;
         }
-
         /// <summary>
         /// Registers a new user into the SafeCity system.
         /// </summary>
@@ -41,6 +40,44 @@ namespace SafeCity.Controllers
                 return StatusCode(500, ErrorMessages.Database.SaveFailed);
             }
         }
+        
+        /// <summary>
+        /// Updates user details by an administrator.
+        /// </summary>
+        /// <param name="user">User details to be updated by admin</param>
+        /// <returns>Returns updated user information</returns>
+        /// <response code="200">User updated successfully</response>
+        /// <response code="400">Invalid request or validation error</response>
+        /// <response code="500">Server error</response>
+        [HttpPut("admin/update")]       
+        [ProducesResponseType(typeof(UserUpdateByAdminResponseDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateUserByAdmin(
+            [FromBody] UserUpdateByAdminRequestDto user)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
 
+                var response = await _userService.UpdateUserByAdmin(user);
+                return Ok(response);
+            }
+            catch (ArgumentNullException)
+            {
+                return BadRequest(new
+                {
+                    error = ErrorMessageUpdate.UserUpdate.RequestNull
+                });
+            }
+
+            catch (Exception ex)
+            {
+                //Throwing Exception
+                return StatusCode(StatusCodes.Status500InternalServerError,ErrorMessageUpdate.User.InternalError);
+            }
+            
+        }
     }
 }
