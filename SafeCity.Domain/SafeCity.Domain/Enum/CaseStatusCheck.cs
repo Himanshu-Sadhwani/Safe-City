0b053738fe1934fa5d7ada23e8fa36bc89ab426c
abhishek-kumar24698e <abhishek.kumar24698e@cognizant.com>
2026-04-10 11:58:11 +0530

    CaseStatus Enum Updated with default value starting from 1 for robust validation

--- SafeCity.Domain/SafeCity.Domain/Enum/CaseStatusCheck.cs
+++ SafeCity.Domain/SafeCity.Domain/Enum/CaseStatusCheck.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text.Json.Serialization;
 
 namespace SafeCity.Domain.Enum
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum CaseStatusCheck
     {
-        Open, Closed, Under_Investigation
+        Open = 1, Closed = 2, Under_Investigation = 3
     }
 }
